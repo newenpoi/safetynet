4102314d6e2e8f1bffd6d51b7ab34783d78c638c
newenpoi <lammenschristopher119@hotmail.com>
2023-02-17 19:38:40 +0100

    first commit

---
+++ src/test/java/com/stargazer/newenpoi/safetynet/SafetynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stargazer.newenpoi.safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
